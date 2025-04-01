2110897ba162a85fd4d032d47ebc312a55ee4914
Minhhau32223 <hau334779@gmail.com>
2025-04-01 19:52:22 +0700

    giao diện play

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -19,6 +19,26 @@ const Player = () => {
                 <img className='w-4 cursor-pointer' src={assets.loop_icon} alt=""/>
 
             </div>
+             <div className=' flex items-center gap-5'>
+             <p>1:06</p>
+             <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer ' >
+                <hr className='bg-green-800 h-1 rounded-full w-[50%]  border-none  w-0'/> 
+                </div>
+                <p>3:06</p>
+              </div>
+             
+        </div>
+        <div className='hidden lg:flex items-center gap-2 opacity-70'>
+            <img src={assets.play_icon} alt="" className='w-4'/>
+            <img src={assets.mic_icon} alt="" className='w-4'/>
+            <img src={assets.queue_iconn} alt="" className='w-4'/>
+            <img src={assets.speaker_icon} alt="" className='w-4'/>
+            <img src={assets.volume_icon} alt="" className='w-4'/>
+            <div className='w-20 bg-slate-50  h-1 rounded'>
+
+            </div>
+            <img src={assets.mini_player_icon} alt="" className='w-4'/>
+            <img src={assets.zoom_icon} alt="" className='w-4'/>
 
         </div>
 
