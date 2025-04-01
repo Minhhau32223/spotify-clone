457bbec4523032e2c6b4a2af8d617bb27077707b
Minhhau32223 <hau334779@gmail.com>
2025-04-01 19:19:57 +0700

    xong phần slibar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Header from './components/Header';
 import Slidebar from './components/Slidebar'
 import "tailwindcss" ;
 
 const App = () => {
   return (
     <div> <div className='bg-black h-screen w-full'>
+    {/* <Header /> */}
     <div className="h-[90%] flex  w-max-[100%]"> 
+    
       <Slidebar />
     </div>
 </div></div>
