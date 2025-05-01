9210f6fdfa5863f6ac98b91d551e1f7b9db58434
hwangtun <tanhoanglevo3110@gmail.com>
2025-05-01 19:09:23 +0700

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "tailwindcss"
 import { FaFacebookF, FaGoogle } from 'react-icons/fa';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 const Login = ({onLogin}) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+          const response = await axios.post('http://127.0.0.1:8000/api/token/', {
+            email,
+            password,
+          });
+      
+          const accessToken = response.data.access;
+          localStorage.setItem('access_token', accessToken);
+      
+          navigate('/DisplayHome.jsx');
+          onLogin();
+          const token = localStorage.getItem('access_token');
+
+            await axios.get('http://127.0.0.1:8000/api/users/', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+            });
+        } catch (error) {
+          alert('Invalid email or password');
+          console.error('Login failed', error);
+        }
+      };
+        
+
+
   return (
     <div className='min-h-screen bg-black text-white flex items-center justify-center'>
         <div className='w-full max-w-md bg-zinc-900 rounded-2xl p-8 shadow-xl'>
@@ -12,34 +46,21 @@ const Login = ({onLogin}) => {
                 <label className="block text-sm text-gray-400 mb-1">Email address or username</label>
                 <input type='email'
                 placeholder='Email address or username ' 
-                className='w-full px-3 py-2 rounded bg-neutral-800 border border-neutral-700 text-white focus:outline-none focus:ring-2 focus:ring-green-500'/>
+                className='w-full px-3 py-2 rounded bg-neutral-800 border border-neutral-700 text-white focus:outline-none focus:ring-2 focus:ring-green-500'
+                onChange={(e) => setEmail(e.target.value)}/>
                 <label className="block text-sm text-gray-400 mb-1">Password</label>
                 <input type='password'
                 placeholder='Enter your password'
+                onChange={(e) => setPassword(e.target.value)}
                 className='w-full px-3 py-2 rounded bg-neutral-800 border border-neutral-700 text-white focus:outline-none focus:ring-2 focus:ring-green-500'/>
 
-                <button className='w-full bg-green-500 hover:bg-green-400 text-black font-semibold py-2 rounded transition-colors' onClick={onLogin}>Log in</button>
+                <button className='w-full bg-green-500 hover:bg-green-400 text-black font-semibold py-2 rounded transition-colors' onClick={handleLogin}>Log in</button>
             </div>
             <div className='mt-6 text-sm flex justify-between items-center'>
                 <a href='#' className='text-zinc-400 hover:underline'>Forgot your password</a>
                 <a href='#' className='text-white hover:underline'>Sign up for Spotify</a>
             </div>
-            <div className="flex items-center mb-6">
-            <div className="flex-grow h-px bg-gray-600"></div>
-            <span className="px-4 text-sm text-gray-400">or</span>
-            <div className="flex-grow h-px bg-gray-600"></div>
-            </div>
-           
-            <div className="space-y-4 mb-6">
-            <button className="w-full flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-500 text-white font-medium py-2 rounded">
-                <FaFacebookF className="text-lg" />
-                Continue with Facebook
-            </button>
-            <button className="w-full flex items-center justify-center gap-2 bg-white text-black hover:bg-gray-200 font-medium py-2 rounded">
-                <FaGoogle className="text-lg" />
-                Continue with Google
-            </button>
-            </div>  
+            
         </div>
     </div>
   )
